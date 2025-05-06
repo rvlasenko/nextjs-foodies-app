f7b4f829b4a490ad9991d8e42e73fd6234805d93
Roman Vlasenko <rvlasenko.1997@gmail.com>
2025-05-06 17:13:25 +0400

    Add meal sharing page with image upload

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetails({ params }) {
   const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
 
   return (
     <>
---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>We could not find the requested food :(</p>
+    </main>
+  );
+}
